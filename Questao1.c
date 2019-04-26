9e02f323489ea7ed363ec7575b7ca3541bea0f89
GledsonReis <50029908+GledsonReis@users.noreply.github.com>
2019-04-26 16:54:11 -0300

    Create Questao1.c

---
+++ Questao1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	
+	float custoFabrica = 10000.00;
+	float percentDistrib = 0.28;
+	float percentImpostos  = 0.45;
+	float custoFinal = custoFabrica + (custoFabrica * percentDistrib) +
+(custoFabrica * percentImpostos);
+
+	printf("Custo de fabricação: %.2f", custoFinal);
+	
+	return 0;
+}
