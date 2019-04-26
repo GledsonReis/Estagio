a745249f1b102474a44b2966481f68b66f31c508
GledsonReis <50029908+GledsonReis@users.noreply.github.com>
2019-04-26 16:58:13 -0300

    Update Questao1.c

--- Questao1.c
+++ Questao1.c
@@ -5,8 +5,7 @@ int main(){
 	float custoFabrica = 10000.00;
 	float percentDistrib = 0.28;
 	float percentImpostos  = 0.45;
-	float custoFinal = custoFabrica + (custoFabrica * percentDistrib) +
-(custoFabrica * percentImpostos);
+	float custoFinal = custoFabrica + (custoFabrica * percentDistrib) + (custoFabrica * percentImpostos);
 
 	printf("Custo de fabricação: %.2f", custoFinal);
 	
